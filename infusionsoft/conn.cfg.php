23aede7cfc020dec8d0949858f92b6caee1f0614
Al Elliott <al@dallasmatthews.co.uk>
2014-03-24 14:17:43 +0100

    Removed connection data

--- infusionsoft/conn.cfg.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$connInfo = array('gymcube:jp174:i:3fec32ad006767472dfdd102bdcff372:This is the connection for jp174.infusionsoft.com');
-
-?>
--- workbench/gymcube_api/includes/messages.php
+++
@@ -1,3 +0,0 @@
-<?php
-if ( isset($r['error']) ) echo '<h4 class="message error">' . $r['error'] . '</h4>';
-if ( isset($r['success']) ) echo '<h4 class="message success">' . $r['success'] . '</h4>';
